196a32f4716f697983fa53f4e10fa4545c8bd332
Marcel Hofer <marcel.hofer@hofstaedtler.com>
2026-03-30 16:26:27 +0200

    moin

--- checkmate-mongodb/run.sh
+++ checkmate-mongodb/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e
+echo "[mongodb] Starting MongoDB..."
+echo "[mongodb] Data directory: /data/db"
 mkdir -p /data/db
-chown -R $(id -u):$(id -g) /data/db
-exec mongod --quiet --bind_ip_all --dbpath /data/db
+ls -la /data/
+echo "[mongodb] Launching mongod..."
+exec mongod \
+    --bind_ip_all \
+    --dbpath /data/db \
+    --nojournal \
+    --setParameter diagnosticDataCollectionEnabled=false
